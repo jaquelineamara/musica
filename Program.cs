52805d81c1414395d6d6dd1d1b3e718e4c7b6207
jaquelineamara <jaquelineamara5@gmail.com>
2022-03-22 21:38:48 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Titúlo: Vício de Amor");
+
+Console.WriteLine(@"Você viu que eu me sairia bem
+Acreditou em mim, disse: Vá além
+Eu quero mais uma vez
+Poder descobrir mais de você
+Agradecer por ser quem é e por ser quem sou");
+
+Console.WriteLine(@"Avião e o compositor
+Difícil explicar
+Ajudar é um vício de amor
+Mas qualquer lugar com você é casa
+Mas qualquer lugar com você é casa");
+
+Console.WriteLine(@"Me olha fundo nos olhos, magia que não sei explicar
+E nem quero entender
+Faz assim, me fala que vai tá sempre aqui
+Mesmo se tudo aqui sucumbir
+É que qualquer lugar com você é casa
+Tudo sem você pra mim não é nada
+Me faz feliz com poucas palavras
+Te dou um mundo, as rosas douradas
+A lua cheia de madrugada
+Te dou minha vida, sem você não valho
+Tempo não volta por mais que eu queira
+Te dou meu tempo, sem você não valho
+Eu não quero viver dessa maneira");
+
+Console.WriteLine(@"Avião e o compositor
+Difícil explicar
+Ajudar é um vício de amor (Vício de amor, vício de amor)
+Avião e o compositor
+Difícil explicar
+Ajudar é um vício de amor (É um vício de amor)
+Mas qualquer lugar com você é casa
+Qualquer lugar (Com você é casa)
+Com você é casa");
+
